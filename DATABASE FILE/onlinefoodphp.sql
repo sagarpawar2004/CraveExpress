3a1b46796d3a6c36e5b8cc8e61b8b7abb5c84279
sagarpawar2004 <sagar1268729@ascs.sies.edu.in>
2023-10-03 23:31:27 +0530

    first commit

---
+++ DATABASE FILE/onlinefoodphp.sql
@@ -0,0 +1,334 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 19, 2023 at 05:31 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onlinefoodphp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `adm_id` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `code` varchar(222) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adm_id`, `username`, `password`, `email`, `code`, `date`) VALUES
+(1, 'admin', '3e010d40e2644662ecc81fc395c38cef', 'admin@mail.com', '', '2023-01-31 11:11:57'),
+(2, 'adarsh', '2710d687c70cd7736b1076a34323c897', 'adarsh@gmail.com', '', '2023-08-19 14:03:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dishes`
+--
+
+CREATE TABLE `dishes` (
+  `d_id` int(222) NOT NULL,
+  `rs_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `slogan` varchar(222) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `img` varchar(222) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dishes`
+--
+
+INSERT INTO `dishes` (`d_id`, `rs_id`, `title`, `slogan`, `price`, `img`) VALUES
+(1, 1, 'Yorkshire Lamb Patties', 'Lamb patties which melt in your mouth, and are quick and easy to make. Served hot with a crisp salad.', '14.00', '62908867a48e4.jpg'),
+(2, 1, 'Lobster Thermidor', 'Lobster Thermidor is a French dish of lobster meat cooked in a rich wine sauce, stuffed back into a lobster shell, and browned.', '36.00', '629089fee52b9.jpg'),
+(3, 4, 'Chicken Madeira', 'Chicken Madeira, like Chicken Marsala, is made with chicken, mushrooms, and a special fortified wine. But, the wines are different;', '23.00', '62908bdf2f581.jpg'),
+(4, 1, 'Stuffed Jacket Potatoes', 'Deep fry whole potatoes in oil for 8-10 minutes or coat each potato with little oil. Mix the onions, garlic, tomatoes and mushrooms. Add yoghurt, ginger, garlic, chillies, coriander', '8.00', '62908d393465b.jpg'),
+(5, 2, 'Pink Spaghetti Gamberoni', 'Spaghetti with prawns in a fresh tomato sauce. This dish originates from Southern Italy and with the combination of prawns, garlic, chilli and pasta. Garnish each with remaining tablespoon parsley.', '21.00', '606d7491a9d13.jpg'),
+(6, 2, 'Cheesy Mashed Potato', 'Deliciously Cheesy Mashed Potato. The ultimate mash for your Thanksgiving table or the perfect accompaniment to vegan sausage casserole. Everyone will love it\'s fluffy, cheesy.', '5.00', '606d74c416da5.jpg'),
+(7, 2, 'Crispy Chicken Strips', 'Fried chicken strips, served with special honey mustard sauce.', '8.00', '606d74f6ecbbb.jpg'),
+(8, 2, 'Lemon Grilled Chicken And Pasta', 'Marinated rosemary grilled chicken breast served with mashed potatoes and your choice of pasta.', '11.00', '606d752a209c3.jpg'),
+(9, 3, 'Vegetable Fried Rice', 'Chinese rice wok with cabbage, beans, carrots, and spring onions.', '5.00', '606d7575798fb.jpg'),
+(10, 3, 'Prawn Crackers', '12 pieces deep-fried prawn crackers', '7.00', '606d75a7e21ec.jpg'),
+(11, 3, 'Spring Rolls', 'Lightly seasoned shredded cabbage, onion and carrots, wrapped in house made spring roll wrappers, deep fried to golden brown.', '6.00', '606d75ce105d0.jpg'),
+(12, 3, 'Manchurian Chicken', 'Chicken pieces slow cooked with spring onions in our house made manchurian style sauce.', '11.00', '606d7600dc54c.jpg'),
+(13, 4, ' Buffalo Wings', 'Fried chicken wings tossed in spicy Buffalo sauce served with crisp celery sticks and Blue cheese dip.', '11.00', '606d765f69a19.jpg'),
+(14, 4, 'Mac N Cheese Bites', 'Served with our traditional spicy queso and marinara sauce.', '9.00', '606d768a1b2a1.jpg'),
+(15, 4, 'Signature Potato Twisters', 'Spiral sliced potatoes, topped with our traditional spicy queso, Monterey Jack cheese, pico de gallo, sour cream and fresh cilantro.', '6.00', '606d76ad0c0cb.jpg'),
+(16, 4, 'Meatballs Penne Pasta', 'Garlic-herb beef meatballs tossed in our house-made marinara sauce and penne pasta topped with fresh parsley.', '10.00', '606d76eedbb99.jpg'),
+(17, 5, 'Chole Samosa', '2 samosa and chole curry with Onion', '70.00', '64e0cfee4df2a.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `remark`
+--
+
+CREATE TABLE `remark` (
+  `id` int(11) NOT NULL,
+  `frm_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `remark` mediumtext NOT NULL,
+  `remarkDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `remark`
+--
+
+INSERT INTO `remark` (`id`, `frm_id`, `status`, `remark`, `remarkDate`) VALUES
+(1, 2, 'in process', 'none', '2022-05-01 05:17:49'),
+(2, 3, 'in process', 'none', '2022-05-27 11:01:30'),
+(3, 2, 'closed', 'thank you for your order!', '2022-05-27 11:11:41'),
+(4, 3, 'closed', 'none', '2022-05-27 11:42:35'),
+(5, 4, 'in process', 'none', '2022-05-27 11:42:55'),
+(6, 1, 'rejected', 'none', '2022-05-27 11:43:26'),
+(7, 7, 'in process', 'none', '2022-05-27 13:03:24'),
+(8, 8, 'in process', 'none', '2022-05-27 13:03:38'),
+(9, 9, 'rejected', 'thank you', '2022-05-27 13:03:53'),
+(10, 7, 'closed', 'thank you for your ordering with us', '2022-05-27 13:04:33'),
+(11, 8, 'closed', 'thanks ', '2022-05-27 13:05:24'),
+(12, 5, 'closed', 'none', '2022-05-27 13:18:03'),
+(13, 10, 'closed', 'we done', '2023-01-31 10:58:27'),
+(14, 11, 'in process', 'ok', '2023-02-01 16:52:23'),
+(15, 11, 'closed', 'ok-', '2023-02-01 16:52:43'),
+(16, 11, 'rejected', 'ok', '2023-02-01 16:52:57'),
+(17, 16, 'in process', 'The order  is on the way', '2023-08-19 14:23:53'),
+(18, 16, 'closed', 'Thank you for ordering from our website', '2023-08-19 14:24:28'),
+(19, 17, 'in process', 'ff', '2023-08-19 15:26:19'),
+(20, 17, 'closed', 'd', '2023-08-19 15:26:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurant`
+--
+
+CREATE TABLE `restaurant` (
+  `rs_id` int(222) NOT NULL,
+  `c_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `phone` varchar(222) NOT NULL,
+  `url` varchar(222) NOT NULL,
+  `o_hr` varchar(222) NOT NULL,
+  `c_hr` varchar(222) NOT NULL,
+  `o_days` varchar(222) NOT NULL,
+  `address` text NOT NULL,
+  `image` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`rs_id`, `c_id`, `title`, `email`, `phone`, `url`, `o_hr`, `c_hr`, `o_days`, `address`, `image`, `date`) VALUES
+(1, 1, 'North Street Tavern', 'nthavern@mail.com', '3547854700', 'www.northstreettavern.com', '8am', '8pm', 'mon-sat', '1128 North St, White Plains', '6290877b473ce.jpg', '2022-05-27 08:10:35'),
+(2, 2, 'Eataly', 'eataly@gmail.com', '0557426406', 'www.eataly.com', '11am', '9pm', 'Mon-Sat', '800 Boylston St, Boston', '606d720b5fc71.jpg', '2022-05-27 08:06:41'),
+(3, 3, 'Nan Xiang Xiao Long Bao', 'nanxiangbao45@mail.com', '1458745855', 'www.nanxiangbao45.com', '9am', '8pm', 'mon-sat', 'Queens, New York', '6290860e72d1e.jpg', '2022-05-27 08:04:30'),
+(4, 4, 'Highlands Bar & Grill', 'hbg@mail.com', '6545687458', 'www.hbg.com', '7am', '8pm', 'mon-sat', '812 Walter Street', '6290af6f81887.jpg', '2022-05-27 11:01:03'),
+(5, 5, 'Gurukrupa', 'guru@gmail.com', '1234567890', 'https://www.youtube.com/', '8am', '9pm', '24hr-x7', 'Plot No 40, Guru Kripa Building, Road No 24, Sion, Mumbai - 400022 (Near Sies College & Railway Station)', '64e0cef175eb1.jpg', '2023-08-19 14:17:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `res_category`
+--
+
+CREATE TABLE `res_category` (
+  `c_id` int(222) NOT NULL,
+  `c_name` varchar(222) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `res_category`
+--
+
+INSERT INTO `res_category` (`c_id`, `c_name`, `date`) VALUES
+(1, 'Continental', '2022-05-27 08:07:35'),
+(2, 'Italian', '2021-04-07 08:45:23'),
+(3, 'Chinese', '2021-04-07 08:45:25'),
+(4, 'American', '2021-04-07 08:45:28'),
+(5, 'Indian', '2023-08-19 14:14:59'),
+(6, 'Japanese', '2023-08-19 14:25:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `u_id` int(222) NOT NULL,
+  `username` varchar(222) NOT NULL,
+  `f_name` varchar(222) NOT NULL,
+  `l_name` varchar(222) NOT NULL,
+  `email` varchar(222) NOT NULL,
+  `phone` varchar(222) NOT NULL,
+  `password` varchar(222) NOT NULL,
+  `address` text NOT NULL,
+  `status` int(222) NOT NULL DEFAULT 1,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`u_id`, `username`, `f_name`, `l_name`, `email`, `phone`, `password`, `address`, `status`, `date`) VALUES
+(7, 'user1', 'z', 'Prince', 'user1@mail.com', '+8801111111111', 'e3738c0d0631f4aaf5c46f62126f1323', 'Dhaka', 1, '2023-08-19 14:27:50'),
+(8, 'amongus', 'Adarsh ', 'Pillai ', 'abc123@gmail.com', '1234567890', '2710d687c70cd7736b1076a34323c897', 'my home', 1, '2023-08-19 13:42:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_orders`
+--
+
+CREATE TABLE `users_orders` (
+  `o_id` int(222) NOT NULL,
+  `u_id` int(222) NOT NULL,
+  `title` varchar(222) NOT NULL,
+  `quantity` int(222) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `status` varchar(222) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_orders`
+--
+
+INSERT INTO `users_orders` (`o_id`, `u_id`, `title`, `quantity`, `price`, `status`, `date`) VALUES
+(1, 4, 'Spring Rolls', 2, '6.00', 'rejected', '2022-05-27 11:43:26'),
+(2, 4, 'Prawn Crackers', 1, '7.00', 'closed', '2022-05-27 11:11:41'),
+(3, 5, 'Chicken Madeira', 1, '23.00', 'closed', '2022-05-27 11:42:35'),
+(4, 5, 'Cheesy Mashed Potato', 1, '5.00', 'in process', '2022-05-27 11:42:55'),
+(5, 5, 'Meatballs Penne Pasta', 1, '10.00', 'closed', '2022-05-27 13:18:03'),
+(6, 5, 'Yorkshire Lamb Patties', 1, '14.00', NULL, '2022-05-27 11:40:51'),
+(7, 6, 'Yorkshire Lamb Patties', 1, '14.00', 'closed', '2022-05-27 13:04:33'),
+(8, 6, 'Lobster Thermidor', 1, '36.00', 'closed', '2022-05-27 13:05:24'),
+(9, 6, 'Stuffed Jacket Potatoes', 1, '8.00', 'rejected', '2022-05-27 13:03:53');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adm_id`);
+
+--
+-- Indexes for table `dishes`
+--
+ALTER TABLE `dishes`
+  ADD PRIMARY KEY (`d_id`);
+
+--
+-- Indexes for table `remark`
+--
+ALTER TABLE `remark`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`rs_id`);
+
+--
+-- Indexes for table `res_category`
+--
+ALTER TABLE `res_category`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`u_id`);
+
+--
+-- Indexes for table `users_orders`
+--
+ALTER TABLE `users_orders`
+  ADD PRIMARY KEY (`o_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `adm_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `dishes`
+--
+ALTER TABLE `dishes`
+  MODIFY `d_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `remark`
+--
+ALTER TABLE `remark`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  MODIFY `rs_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `res_category`
+--
+ALTER TABLE `res_category`
+  MODIFY `c_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `u_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users_orders`
+--
+ALTER TABLE `users_orders`
+  MODIFY `o_id` int(222) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ about.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
+<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+
+      <link rel="stylesheet" href="css/login.css">
+
+	  <style type="text/css">
+	  #buttn{
+		  color:#fff;
+		  background-color: #5c4ac7;
+	  }
+      body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        .container {
+            max-width: 1000px;
+            margin: 20 auto;
+        }
+        h1 {
+            text-align: center;
+        }
+	  </style>
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  
+</head>
+
+<body>
+<body>
+    
+    <header id="header" class="header-scroll top-header headrom">
+        <nav class="navbar navbar-dark">
+            <div class="container">
+                <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/logo.jpg" alt="" width="102px" height="50px"> </a>
+                <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                        <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                        <li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders <span class="sr-only"></span></a> </li>
+                    
+                      <!--  <li class="nav-item"><a href="login.php" class="nav-link active">Login <span class="sr-only"></span></a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register <span class="sr-only"></span></a> </li>
+    -->
+							 
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <div style=" background-image: url('images/img/bg2.jpg');">
+    <div class="container">
+    
+        <h1>&nbsp;</h1>
+        <h1>&nbsp;</h1>
+        <h1 style="color: red;">About Us</h1>
+        <p style="color: black;"><b>Welcome to Food Delivery System, your go-to destination for delicious meals delivered right to your doorstep. We are passionate about food and convenience, and our mission is to provide you with the best dining experience from the comfort of your home or office.</b></p>
+
+        <h2 style="color: red;">Our Mission</h2>
+        <p style="color: black;"><b>At Food Delivery System, we are committed to delivering quality food that satisfies your cravings. We work with local restaurants and chefs to bring you a diverse menu of mouthwatering dishes. Our mission is to make mealtime hassle-free, so you can focus on what you love.</b></p>
+
+        <h2 style="color: red;">Our Team</h2>
+        <p style="color: black;"><b>We have a dedicated team of food enthusiasts, delivery drivers, and customer support professionals who work tirelessly to ensure your orders are delivered promptly and your questions are answered.</b></p>
+        
+        <p style="color: black;"><b>Meet our team:</b></p>
+         <ol style="color: black;">
+        <li><b><ins>Maya Nair</ins> - Project Advisior</b></li>
+         </ol>
+<b>
+        <ol style="color: black;">
+        <li>Project Designer:-</li>
+          <ol >
+             <li> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      1. Sagar Pawar  </li>
+            <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2.Adarsh Pillai </li>
+             <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 3.Sakshi Pol   </li>
+         </ol> 
+        </ol></b>
+
+
+         
+
+        <p style="color: black;"><b>Thank you for choosing Food Delivery System. We look forward to serving you and providing you with a delightful culinary experience. If you have any feedback or questions, please don't hesitate to contact us. </b></p>
+    </div></div>
+
+
+  
+
+
+
+  
+
+   
+  
+
+  <footer class="footer">
+            <div class="container">
+                
+          
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                        <img src="images/logo.jpg" width="300px" height="150px">
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray" margin-right="30px">
+                                    <h5>Office Address</h5>
+                                   <p>401 Haware Business Park <br>
+                                      Vashi, Navi Mumbai</p>
+                                    <h5>Phone: +91 9856327891</a></h5> 
+                                        </div>
+                        <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                                <h5>Follow Us</h5>
+                                <ul>
+                                    <li><a href="#">Instagram</a></li>
+                                    <li><a href="#">Facebook</a></li>
+                                    <li><a href="#">YouTube</a></li>
+                                    <li><a href="#">Twitter</a></li>
+                                </ul>
+                        </div>
+
+                    </div>
+                </div>
+          
+            </div>
+        </footer>
+    
+
+
+</body>
+
+</html>
